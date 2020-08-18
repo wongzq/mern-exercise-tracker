640e22e94ffb27a09db5600325871f2fcd61c512
WONG ZHENG QUAN <38060298+wongzhengquan@users.noreply.github.com>
2020-08-18 14:23:59 +0800

    MERN Tutorial 30:50

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// use Express app
+app.use(cors())
+app.use(express.json())
+
+// connecto to MongoDB
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+const connection = mongoose.connection
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully")
+})
+
+// router
+app.use('/exercises', exerciseRouter)
+app.use('/users', usersRouter)
+
+// listen to a port
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
